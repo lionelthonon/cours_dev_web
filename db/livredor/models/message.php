847310592b243ad7b8aaf4ff60a4f1e1675cd1cf
Lionel Thonon <lionel.thonon@gmail.com>
2015-02-12 11:53:33 +0100

    Récupère les messages dans la BDD

--- db/livredor/models/message.php
+++ db/livredor/models/message.php
@@ -1,7 +1,8 @@
 <?php
+// Récupérer les messages dans la BDD
 function getMessages($dbConnexion) {
 
-    $sql = 'SELECT * FROM messages';
-
-    return ;
+    $sql = 'SELECT * FROM messages'; // Stocker la requête sql
+    $res = $dbConnexion->query($sql); // Résultat de la requête -> Voir PDO::query
+    return $res->fetchAll();
 }
