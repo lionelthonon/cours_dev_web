3cbe6e7797b5fcd37284235b10b8cfb6f669fa74
Lionel Thonon <lionel.thonon@gmail.com>
2015-02-12 12:09:48 +0100

    config PDO

--- db/livredor/configs/db.php
+++ db/livredor/configs/db.php
@@ -3,3 +3,9 @@
 define('DSN', 'mysql:host=localhost;dbname=golden');
 define('USER', 'root');
 define('PSSWD', '');
+
+/* --- Constantes de classes -- configuration de PDO --- */
+$options = [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+];
