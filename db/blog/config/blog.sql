1508e22a5f3bb8603adbe220bd18583ebe5b37d3
lionelthonon <lionel.thonon@gmail.com>
2015-02-22 18:56:10 +0100

    Travail terminé -- Ajout du fichier d'import SQL

--- db/blog/config/blog.sql
+++ db/blog/config/blog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 19 Février 2015 à 02:30
+-- Généré le :  Dim 22 Février 2015 à 18:55
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -53,19 +53,9 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `content` text NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `author_sign` varchar(200) NOT NULL,
-  `id_cat` tinyint(3) unsigned NOT NULL,
+  `id_cat` tinyint(3) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
-
---
--- Contenu de la table `posts`
---
-
-INSERT INTO `posts` (`id`, `content`, `date`, `author_sign`, `id_cat`) VALUES
-(1, 'J''ai été forcé d''aller voir 50 Shades of Grey ... ', '2015-02-19 01:00:08', 'Lio Ladmin', 2),
-(2, 'Le dernier album de Stromae est pas terrible ...', '2015-02-19 01:00:45', 'Lio Ladmin', 1),
-(3, 'Je ne trouve plus de vidéos de chats !', '2015-02-19 01:01:17', 'Lio Ladmin', 3),
-(4, 'J''ai fais des crêpes à la chandeleur !', '2015-02-19 01:01:51', 'Lio Ladmin', 4);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
